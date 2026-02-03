efd0d5ad937544d9829b461aa882ed3c64c10b95
mandrewcito <anbaalo@gmail.com>
2026-02-03 18:24:37 +0100

    feat(azure): azure serverless function

---
+++ src/SignalRAzure/messages/__init__.py
@@ -0,0 +1,12 @@
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> str:
+    message = req.get_json()
+    return json.dumps({
+        'target': 'newMessage',
+        'arguments': [
+            message
+            ]
+    })
---
+++ src/SignalRAzure/negotiate/__init__.py
@@ -0,0 +1,11 @@
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, connectionInfoJson: str) -> func.HttpResponse:
+    return func.HttpResponse(
+        connectionInfoJson,
+        status_code=200,
+        headers={
+            'Content-type': 'application/json'
+        }
+    )
