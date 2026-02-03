772436a37c42a4771df3fe99309b013120613753
mandrewcito <anbaalo@gmail.com>
2026-02-03 21:44:19 +0100

    feat(azure): check azure  connection string

--- src/SignalRAzure/health/__init__.py
+++ src/SignalRAzure/health/__init__.py
@@ -4,14 +4,14 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    connection_string = os.getenv("SIGNALRCORE_AZURE_CONNECTION_STRING", None)
+    connection_string = os.getenv("AzureSignalRConnectionString", None)
 
     if connection_string is None or len(connection_string.strip()) == 0:
         return func.HttpResponse(
             json.dumps({
                 "status": "fail",
                 "env": {
-                    "SIGNALRCORE_AZURE_CONNECTION_STRING": connection_string
+                    "AzureSignalRConnectionString": connection_string
                 }
             }), mimetype='application/json')
 
