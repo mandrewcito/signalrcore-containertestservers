c07e16075f98a52c95f94ec268a944559605a2b5
mandrewcito <anbaalo@gmail.com>
2026-02-04 11:58:46 +0100

    add health endpoint to signalr server

---
+++ src/SignalRSampleServer/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿namespace SignalRSample.Controllers
+{
+	using System.Diagnostics;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.Extensions.Logging;
+	using Models;
+
+	public class HealthController : Controller
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok(new {
+             status="ok"   
+            });
+        }
+    }
+}
